1ae4afa4d85781d07372dff19d98ec5d99cf6a89
A P <67877471+beerded@users.noreply.github.com>
2026-04-03 23:49:30 +0300

    Add Readiness check, refactor
    * Add a readiness check at /healthz that just returns 200 when the page
      is up
    * Serve all the files under the current path still, but update the path
      of the filehandler to use "/app/" instead of "/"

--- main.go
+++ main.go
@@ -7,15 +7,23 @@ import (
 
 func main() {
 	const port = "8080"
+
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	server := &http.Server{
 		Addr:		":"+port,
 		Handler:	mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
 	log.Printf("Starting server on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
