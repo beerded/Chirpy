0b1e43b68b71009863a8c4b0ba14cad49c94548d
A P <67877471+beerded@users.noreply.github.com>
2026-04-07 22:04:05 +0300

    Add function for making and validating JWTs
    When a JWT is validated, it should return a UUID equal to the UUID that
    was used in the preceeding MakeJWT call

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,16 @@
 package auth
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"github.com/alexedwards/argon2id"
 )
+
+type TokenType string
+const TokenTypeAccess TokenType = "chirpy-access"
 
 func HashPassword(password string) (string, error) {
 	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -18,4 +26,51 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 		return false, err
 	}
 	return res, err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:		string(TokenTypeAccess),
+		IssuedAt:	jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt:	jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:	userID.String(),
+	})
+	ss, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return ss, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claimsStruct := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claimsStruct,
+		func(token *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, fmt.Errorf("invalid issuer: %v", issuer)
+	}
+
+	myUUID, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid user ID: %w", err)
+	}
+	return myUUID, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,8 +1,13 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func TestSmoketest(t *testing.T) {
+	"github.com/google/uuid"
+)
+
+func TestLogin(t *testing.T) {
 	password1 := "correctPassword"
 	password2 := "anotherCorrectPassword"
 	hash1, _ := HashPassword(password1)
@@ -87,6 +92,69 @@ func TestNegativeCase(t *testing.T) {
 		actual, err := CheckPasswordHash("some other password", hash)
 		if actual != c.expected {
 			t.Errorf("Hashes are expected to be different but they somehow still match")
+		}
+	}
+}
+func TestJWTS(t *testing.T) {
+	secretString1 := "foobar"
+	secretString2 := "barfoo"
+	uuid1, _ := uuid.NewRandom()
+	uuid2, _ := uuid.NewRandom()
+	token1, _ := MakeJWT(uuid1, secretString1, 10 * time.Minute)
+	token2, _ := MakeJWT(uuid2, secretString2, -10 * time.Minute)
+	t.Logf("token1: %s", token1)
+	t.Logf("token2: %s", token2)
+
+	cases := []struct {
+		testName	string
+		secret		string
+		uuid		uuid.UUID
+		token		string
+		wantUUID	uuid.UUID
+		wantErr		bool
+	}{
+		{
+			testName:		"Correct Secret",
+			secret:			secretString1,
+			uuid:			uuid1,
+			token:			token1,
+			wantUUID:		uuid1,
+			wantErr:		false,
+		},
+		{
+			testName:		"Incorrect Secret",
+			secret:			"wrong secret",
+			uuid:			uuid1,
+			token:			token1,
+			wantUUID:		uuid.Nil,
+			wantErr:		true,
+		},
+		{
+			testName:		"Expired Token",
+			secret:			secretString2,
+			uuid:			uuid2,
+			token:			token2,
+			wantUUID:		uuid.Nil,
+			wantErr:		true,
+		},
+		{
+			testName:		"Invalid Token",
+			secret:			secretString1,
+			uuid:			uuid1,
+			token:			"invalid.token",
+			wantUUID:		uuid.Nil,
+			wantErr:		true,
+		},
+	}
+
+	for _, c := range cases {
+		ss, err := ValidateJWT(c.token, c.secret)
+		if (err != nil) != c.wantErr {
+			t.Errorf("TEST %v - ValidateJWT() had err = %v, want err = %v\n", c.testName, err, c.wantErr)
+			return
+		}
+		if ss != c.wantUUID {
+			t.Errorf("TEST %v - ValidateJWT() wanted UUID %v, got %v\n", c.testName, c.wantUUID, ss)
 		}
 	}
 }
